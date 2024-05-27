7a84f157ecf277f7310d49942932c1f5b2b5e7c3
Cesar Londono <cesar28232014@gmail.com>
2024-05-26 19:52:27 -0500

    Clase 2

---
+++ Clase_Dos/vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Artista Favorito</h1>
+      <img src="https://i.guim.co.uk/img/media/b71f2a9c55b1c0ba22ae2b64f985451208c6b7aa/0_546_8192_4918/master/8192.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=ae1c2ecbfb827ffedea6f5553cdd44da" alt="BMTH"/>
+      <p> Bring Me the Horizon are a British rock band, formed in Sheffield in 2004. The group currently consists of lead vocalist Oli Sykes, drummer Matt Nicholls, guitarist Lee Malia and bassist Matt Kean. They are signed to RCA Records globally and Columbia Records exclusively in the United States. 
+      </p>
+    </div>
+  );
+}
+
+export default App;
